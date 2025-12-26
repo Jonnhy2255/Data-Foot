2973d3a2eb18ceea1144ee3d592ce3184caeec17
Jonnhy2255 <billionsjonnhy@gmail.com>
2025-12-26 01:14:34 +0000

    Add function to list JSON files in data directory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+function listLeagues() {
+  const dataDir = path.join(__dirname, "..", "data");
+  return fs.readdirSync(dataDir).filter(file => file.endsWith(".json"));
+}
+
+module.exports = { listLeagues };
 
